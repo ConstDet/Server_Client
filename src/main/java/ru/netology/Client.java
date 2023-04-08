d82a52d33092a7443cb1d3ecb9fbbbe77e133ddb
ConstDet <constx7@gmail.com>
2023-04-08 15:14:31 +0300

    Создал клиента

--- src/main/java/ru/netology/Client.java
+++ src/main/java/ru/netology/Client.java
@@ -1,7 +1,25 @@
 package ru.netology;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final int PORT = 8086;
+        final String HOST = "localhost";
+        try (Socket clientSocket = new Socket(HOST, PORT);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            System.out.println(String.format("Подключились на %d порту", clientSocket.getPort()));
+            out.println("Константин");
+            String reception = in.readLine();
+            System.out.println(reception);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -11,16 +11,17 @@ public class Server {
     public static void main(String[] args) {
         final int PORT = 8086;
         try (ServerSocket serverSocket = new ServerSocket(PORT)) { // открыли порт для приема сообщений
-            try (Socket clientSocket = serverSocket.accept(); // ждем подключения
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                System.out.println("Принято новое соединение!");
-                final String name = in.readLine();
-                out.println(String.format("Привет %s, твой порт номер 5d", name, clientSocket.getPort()));
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept(); // ждем подключения
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    System.out.println("Принято новое соединение!");
+                    final String name = in.readLine();
+                    out.println(String.format("Привет %s, твой порт номер %d", name, clientSocket.getPort()));
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Hello world!");
     }
 }
